49cd58d0201d8325834638996984b44ae1c77ec5
“Jackdream” <821168115@qq.com>
2025-12-18 15:54:13 +0800

    更新数据集

---
+++ chart/chart_gold_chat.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 设置中文字体
+plt.rcParams['font.sans-serif'] = ['SimHei', 'Microsoft YaHei', 'DejaVu Sans']  # 指定默认字体
+plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
+
+# 从CSV文件读取
+chat_count = pd.read_csv('./data/chat_count_day.csv')
+gold_price = pd.read_csv('./data/gold_price.csv')
+print(chat_count)
+print(gold_price)
+
+merged_df = pd.merge(chat_count, gold_price, on='date', how='outer')
+print(merged_df)
+
+fig, ax1 = plt.subplots(figsize=(12, 6))
+
+# 第一个Y轴（左侧）- 温度
+color1 = 'tab:red'
+ax1.set_xlabel('日期')
+ax1.set_ylabel('聊天次数', color=color1, fontsize=12)
+line1 = ax1.plot(merged_df['date'], merged_df['count'], color=color1, marker='s', linewidth=2, label='聊天次数')
+ax1.tick_params(axis='y', labelcolor=color1)
+
+# 第二个Y轴（右侧）- 销售额
+ax2 = ax1.twinx()  # 创建共享x轴的第二个y轴
+color2 = 'tab:blue'
+ax2.set_ylabel('黄金价格', color=color2, fontsize=12)
+line2 = ax2.plot(merged_df['date'], merged_df['price'], color=color2, marker='s', linewidth=2, label='黄金价格')
+ax2.tick_params(axis='y', labelcolor=color2)
+
+plt.title('聊天次数与黄金价格对比', fontsize=14, fontweight='bold')
+plt.grid(True, alpha=0.3)
+
+# 合并图例
+lines1, labels1 = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left')
+# ax1.legend(lines1, labels1, loc='upper left')
+
+plt.tight_layout()
+plt.show()
---
+++ chart/chat.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+from io import StringIO
+
+
+response = requests.get('http://localhost:5030/api/v1/chatlog?time=2025-12-17&talker=%E9%B8%BF%E5%AD%A6%E9%99%A2%E7%9F%AD%E7%BA%BF%E4%BA%A4%E6%B5%81%E7%BE%A4&format=csv')
+print(response.text)
+
+
+# 从CSV文件读取
+# chat = pd.read_csv('./data/wechat_chat/chat_20251217.csv')
+chat = pd.read_csv(StringIO(response.text))
+print(len(chat))
