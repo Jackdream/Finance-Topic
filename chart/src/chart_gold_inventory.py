6314796c92e42a43eac9d2bbae556f11463dbb8e
Jackdream <821168115@qq.com>
2025-12-28 23:17:11 +0800

    提交页面

--- chart/src/chart_gold_inventory.py
+++ chart/src/chart_gold_inventory.py
@@ -17,40 +17,72 @@ inventory3 = pd.read_csv('../data/shfe_gold_inventory.csv', parse_dates=['date']
 inventory4 = pd.read_csv('../data/spdr_gold_inventory.csv', parse_dates=['date'])
 # 中国黄金ETF库存
 inventory5 = pd.read_csv('../data/sge_gold_etf_inventory.csv', parse_dates=['date'])
+# 中国央行黄金库存
+inventory6 = pd.read_csv('../data/pboc_gold.csv', parse_dates=['date'])
+# 全球央行黄金库存
+inventory7 = pd.read_csv('../data/global_central_bank_gold_reserves.csv', parse_dates=['date'])
 
-inventory1 = inventory1.loc[inventory1.index.max()].to_frame().T
+print(['date'])
+# 拿最新数据
+inventoryLine1 = inventory1.loc[inventory1.index.max()]
+name1 = "COMEX库存(t)("  + inventoryLine1['date'].strftime("%Y-%m-%d") + ")"
+inventory1 = inventoryLine1.to_frame().T
 inventory1['date'] = pd.to_datetime(inventory1['date'])
 inventory1 = inventory1.reset_index(drop=True)
-inventory1 = inventory1.rename(columns={'inventory(t)': 'COMEX库存(t)'})
+inventory1 = inventory1.rename(columns={'inventory(t)': name1})
 
-inventory2 = inventory2.loc[inventory2.index.max()].to_frame().T
+inventoryLine2 = inventory2.loc[inventory2.index.max()]
+name2 = "LBMA库存(t)("  + inventoryLine2['date'].strftime("%Y-%m-%d") + ")"
+inventory2 = inventoryLine2.to_frame().T
 inventory2['date'] = pd.to_datetime(inventory2['date'])
 inventory2 = inventory2.reset_index(drop=True)
-inventory2 = inventory2.rename(columns={'inventory(t)': 'LBMA库存(t)'})
+inventory2 = inventory2.rename(columns={'inventory(t)': name2})
 
-inventory3 = inventory3.loc[inventory3.index.max()].to_frame().T
+inventoryLine3 = inventory3.loc[inventory3.index.max()]
+name3 = "上期所库存(kg)("  + inventoryLine3['date'].strftime("%Y-%m-%d") + ")"
+inventory3 = inventoryLine3.to_frame().T
 inventory3['date'] = pd.to_datetime(inventory3['date'])
 inventory3 = inventory3.reset_index(drop=True)
-inventory3 = inventory3.rename(columns={ 'inventory(kg)': '上期所库存(kg)'})
+inventory3 = inventory3.rename(columns={ 'inventory(kg)': name3})
 
-inventory4 = inventory4.loc[inventory4.index.max()].to_frame().T
+inventoryLine4 = inventory4.loc[inventory4.index.max()]
+name4 = "SPDR-ETF库存(t)("  + inventoryLine4['date'].strftime("%Y-%m-%d") + ")"
+inventory4 = inventoryLine4.to_frame().T
 inventory4['date'] = pd.to_datetime(inventory4['date'])
 inventory4 = inventory4.reset_index(drop=True)
-inventory4 = inventory4.rename(columns={ '库存(t)': 'SPDR-ETF库存(t)'})
+inventory4 = inventory4.rename(columns={ '库存(t)': name4})
 
-inventory5 = inventory5.loc[inventory5.index.max()].to_frame().T
+inventoryLine5 = inventory5.loc[inventory5.index.max()]
+name5 = "中国黄金ETF库存(t)("  + inventoryLine5['date'].strftime("%Y-%m-%d") + ")"
+inventory5 = inventoryLine5.to_frame().T
 inventory5['date'] = pd.to_datetime(inventory5['date'])
 inventory5 = inventory5.reset_index(drop=True)
-inventory5 = inventory5.rename(columns={'inventory(t)': '中国黄金ETF(t)'})
+inventory5 = inventory5.rename(columns={'inventory(t)': name5})
 
-merged_df = pd.concat([inventory1, inventory2, inventory3, inventory4, inventory5], axis=1)
+inventoryLine6 = inventory6.loc[inventory6.index.max()]
+name6 = "中国央行黄金库存(t)("  + inventoryLine6['date'].strftime("%Y-%m-%d") + ")"
+inventory6 = inventoryLine6.to_frame().T
+inventory6['date'] = pd.to_datetime(inventory6['date'])
+inventory6 = inventory6.reset_index(drop=True)
+inventory6 = inventory6.rename(columns={'黄金储备(t)': name6})
+
+inventoryLine7 = inventory7.loc[inventory6.index.max()]
+name6 = "中国央行黄金库存(t)("  + inventoryLine7['date'].strftime("%Y-%m-%d") + ")"
+inventory6 = inventoryLine6.to_frame().T
+inventory6['date'] = pd.to_datetime(inventory6['date'])
+inventory6 = inventory6.reset_index(drop=True)
+inventory6 = inventory6.rename(columns={'黄金储备(t)': name6})
+
+# 库存相加
+
+merged_df = pd.concat([inventory1, inventory2, inventory4, inventory3, inventory5, inventory6], axis=1)
 # merged_df = pd.merge(inventory1, inventory2, on='date', how='outer')
 # merged_df = pd.merge(merged_df, inventory3, on='date', how='outer')
 # merged_df = pd.merge(merged_df, inventory4, on='date', how='outer')
 # merged_df = pd.merge(merged_df, inventory5, on='date', how='outer')
 # merged_df = merged_df.rename(columns={'inventory(t)_x': 'COMEX库存(t)','inventory(kg)': '上期所库存(kg)', 
 # 'inventory(t)_y': 'LBMA库存(t)', '库存(t)': 'SPDR-ETF库存(t)', 'inventory(t)': '中国黄金ETF(t)'})
-merged_df = merged_df.drop(columns=['inventory(oz)'])
+merged_df = merged_df.drop(columns=['inventory(oz)', 'date', '黄金储备(oz)'])
 print(merged_df)
 html_table = merged_df.to_html(
     index=False,
