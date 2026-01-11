011a06ea6e5befa6473c49542cae68c603a9e0f7
Jackdream <821168115@qq.com>
2026-01-12 07:47:25 +0800

    更新数据，更换目录

--- chart/src/spot_future_spread.py
+++ chart/src/spot_future_spread.py
@@ -8,12 +8,12 @@ plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 
 # 从CSV文件读取
 price = pd.read_csv('../data/黄金价格.csv')
-price = price[(price['日期'] >= '2026-01-05') & (price['日期'] <= '2026-01-08')]
+price = price[(price['日期'] >= '2026-01-05') & (price['日期'] <= '2026-01-09')]
 price['价差'] = price['COMEX黄金价格'] - price['LBMA黄金价格']
-result = price['价差'].sum() / 4
+result = price['价差'].sum() / 5
 
 print(result.round(2))
 
-result = f"2026-01-08,{result.round(2)}"
+result = f"2026-01-09,{result.round(2)}"
 with open('./output/黄金期现价差.csv', 'w', encoding='utf-8') as f:
     f.write(result)
