911c9e75a22200f552043f56b343de153950fe23
“Jackdream” <821168115@qq.com>
2026-01-16 20:01:22 +0800

    更新数据

--- report/src/report.py
+++ report/src/report.py
@@ -1,9 +1,27 @@
 import pandas as pd
-
+startDate =  '2026-01-05'
+endDate =  '2026-01-09'
 # 从CSV文件读取
-inventory = pd.read_csv('../data/shfe_gold_inventory.csv')
-print(inventory)
-html_table = inventory.to_html(
+pd1 = pd.read_csv('../data/黄金价格.csv')
+pd1 = pd1[(pd1['日期'] >= startDate) & (pd1['日期'] <= endDate)]
+
+pd2 = pd.read_csv('../data/COMEX黄金库存.csv')
+pd2 = pd2[(pd2['日期'] >= startDate) & (pd2['日期'] <= endDate)]
+pd2 = pd2.drop(columns=['库存(盎司)','注册黄金(盎司)','合格黄金(盎司)'])
+
+pd3 = pd.read_csv('../data/SPDR黄金ETF库存.csv')
+pd3 = pd3[(pd3['日期'] >= startDate) & (pd3['日期'] <= endDate)]
+pd3 = pd3.drop(columns=['增持/减持(吨)','总价值(美元)'])
+
+merged_df = pd.merge(pd2, pd3, on='日期', how='outer')
+html_table1 = pd1.to_html(
+    index=False,
+    classes='product-table',
+    border=0,
+    justify='left'
+)
+
+html_table2 = merged_df.to_html(
     index=False,
     classes='product-table',
     border=0,
@@ -47,11 +65,15 @@ full_html = f"""
     </style>
 </head>
 <body>
-    <h1>产品库存清单</h1>
-    {html_table}
+    <h1>黄金整体存量</h1>
+    <h1>黄金库存</h1>
+    {html_table2}
+    <h1>黄金成交量</h1>
+    <h1>黄金价格</h1>
+    {html_table1}
 </body>
 </html>
 """
 
-with open('./output/product_inventory.html', 'w', encoding='utf-8') as f:
+with open('./output/report_2026_01_09.html', 'w', encoding='utf-8') as f:
     f.write(full_html)
