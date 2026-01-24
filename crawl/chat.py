82e3b4f32e0f41f87b2931e713ce9e18b91c7e94
“Jackdream” <821168115@qq.com>
2026-01-24 09:58:12 +0800

    更新数据

--- crawl/chat.py
+++ crawl/chat.py
@@ -4,7 +4,7 @@ import requests
 from io import StringIO
 
 
-response = requests.get('http://localhost:5030/api/v1/chatlog?time=2026-01-14&talker=%E9%B8%BF%E5%AD%A6%E9%99%A2%E7%9F%AD%E7%BA%BF%E4%BA%A4%E6%B5%81%E7%BE%A4&format=csv')
+response = requests.get('http://localhost:5030/api/v1/chatlog?time=2026-01-22&talker=%E9%B8%BF%E5%AD%A6%E9%99%A2%E7%9F%AD%E7%BA%BF%E4%BA%A4%E6%B5%81%E7%BE%A4&format=csv')
 print(response.text)
 
 
